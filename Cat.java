0dce595e11c92c54ed8ab5ea06c52e21cdfc8630
BM <bm1120836@yandex.ru>
2023-01-22 12:10:48 +0300

    Класс Cat

---
+++ Cat.java
@@ -0,0 +1,31 @@
+
+public class Cat extends Animal implements Runable{
+    private String colour;
+    
+
+    public Cat(String name, int box, String color) {
+        super(name, box);
+        this.colour = colour;
+    }
+
+    @Override
+    public String say() {
+        return "Мяу";
+    }
+
+    @Override
+    public String toString() {
+        return "Cat" + super.toString();
+    }
+
+    @Override
+    public int speedOfRun() {
+        // TODO Auto-generated method stub
+        return 15;
+    }
+
+
+
+
+    
+}
